001d618acf87693eaa46ceed5e20dc37c91aa0b9
Sihayah <sihaya_a@hotmail.com>
2021-11-02 19:01:35 -0400

    last touches

--- assets/js/script.js
+++ assets/js/script.js
@@ -75,12 +75,19 @@ $(".card .list-group").sortable({
   tolerance: "pointer",
   helper: "clone",
   activate: function(event, ui) {
+    $(this).addClass("dropover");
+    $(".bottom-trash").addClass("bottom-trash-drag");
   },
   deactivate: function(event, ui) {
+    $(this).removeClass("dropover");
+    $(".bottom-trash").removeClass("bottom-trash-drag");
   },
   over: function(event) {
+    $(event.target).addClass("dropover-active");
+    $("bottom-trash").addClass("bottom-trash-active");
   },
   out: function(event) {
+    $(event.target).removeClass("dropover-active");
   },
   update: function() {
     var tempArr = [];
@@ -126,8 +133,10 @@ $("#trash").droppable({
 
   },
   over: function(event, ui) {
+    $(".bottom-trash").addClass("bottom-trash-active");
   },
   out: function(event, ui) {
+    $(".bottom-trash").removeClass("bottom-trash-active");
   }
 });
 
@@ -145,7 +154,7 @@ $("#task-form-modal").on("shown.bs.modal", function() {
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function() {
+$("#task-form-modal .btn-save").click(function() {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
@@ -294,3 +303,8 @@ $(".list-group-item").on("click", "span", function() {
 
 });
 
+setInterval(function () {
+  $(".card .list-group-item").each(function(index, el) {
+    auditTask(el);
+  });
+}, (1000*60)*30);
